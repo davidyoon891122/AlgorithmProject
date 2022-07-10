5983602c9ec7b5c9ec64a4fd463b46f1b52059fb
davidyoon <davidyoon@lotte.net>
2022-07-10 20:36:14 +0900

    Add conditions on README.md

--- day1/twoSumLoop.swift
+++ day1/twoSumLoop.swift
@@ -1,3 +1,10 @@
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    
+    for index in 0...nums.count {
+        for nextIndex in 1...nums.count {
+            if nums[index] + nums[nextIndex] == target {
+                return [index, nextIndex]
+            }
+        }
+    }
+    return []
 }
